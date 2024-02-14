ae1c38909b7430887be7693800f2720948abe451
Ggaabriel <djskilleton@gmail.com>
2024-02-14 05:17:53 +0200

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
- /** @type {import('next').NextConfig} */ 
- const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    }
-    module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+};
+
+export default nextConfig;
